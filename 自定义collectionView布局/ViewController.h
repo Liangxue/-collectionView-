f2256025be0c979140c5b7aecb1a5952d94bd55d
Liangxue <15145078036@163.com>
2015-10-09 17:30:47 +0800

    Initial Commit

---
+++ 自定义collectionView布局/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  自定义collectionView布局
+//
+//  Created by ma c on 15/10/9.
+//  Copyright (c) 2015年 梁学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 自定义collectionView布局/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  自定义collectionView布局
+//
+//  Created by ma c on 15/10/9.
+//  Copyright (c) 2015年 梁学. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
